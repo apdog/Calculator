b29e4bf04d51f697882ed8edc89cdab436fe7bb3
apdog <alex.puhow@gmail.com>
2024-02-04 10:14:21 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        while (true) {
+            Scanner in = new Scanner(System.in);
+            String a = in.nextLine();
+            String b = in.nextLine();
+            System.out.println(a+b);
+        }
+
+    }
+}
